e03081bd5d24b152b001fa799864d6693e1b75cf
Tyler Sommer <sommertm@gmail.com>
2014-02-25 19:53:58 -0800

    Added license blurbs to files

--- src/Benchmark/Benchmark.php
+++ src/Benchmark/Benchmark.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Copyright (c) Tyler Sommer
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace TylerSommer\Nice\Benchmark;
 
 use TylerSommer\Nice\Benchmark\ResultPrinter\SimplePrinter;
--- src/Benchmark/Test/CallableTest.php
+++ src/Benchmark/Test/CallableTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Copyright (c) Tyler Sommer
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace TylerSommer\Nice\Benchmark\Test;
 
 class CallableTest extends AbstractTest
